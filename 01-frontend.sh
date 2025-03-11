6cb3a2505b423ea674f9b268e22705be16ca8ef1
YashavanthaN8057 <yme1061920@gmail.com>
2025-03-12 00:13:12 +0530

    doing exercise on res

--- 01-frontend.sh
+++ 01-frontend.sh
@@ -2,9 +2,16 @@
 
 echo "the exit code is $(id -u)"
 
+# if [ $(id -u) -ne 0 ]; then 
+#     echo -e "\e[31m You should be root user to perform this script \e[0m"
+#     echo -e "Example usage: \n\t \e[35m sudo bash $0 \e[0m"
+#     exit 2
+# fi
+
+
 if [ $(id -u) -ne 0 ]; then 
     echo -e "\e[31m You should be root user to perform this script \e[0m"
-    echo -e "Example usage: \n\t \e[35m sudo bash $0 \e[0m"
-    exit 2
-fi
-
+    
+    else echo "this is root user"
+    exit 3
+ fi

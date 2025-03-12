7dd07f95ac2b9589967c0a77aacfdbe1febd0195
YashavanthaN8057 <yme1061920@gmail.com>
2025-03-13 00:03:34 +0530

    doing exercise on if cond

--- 01-frontend.sh
+++ 01-frontend.sh
@@ -1,5 +1,52 @@
 #!/bin/bash
 
+#!/bin/bash
+
+component=frontend
+logFile=/tmp/$component.log
+# Common function to print the status of the component
+if [ $(id -u) -ne 0 ]; then 
+    echo -e "\e[31m You should be root user to perform this script \e[0m"
+    echo -e "Example usage: \n\t \e[35m sudo bash $0 \e[0m"
+    exit 2
+fi
+
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m"    
+    else
+        echo -e "\e[31m Failure \e[0m"    
+    fi
+}
+
+echo -n "Installing Nginx:"
+dnf install nginx -y     &>> $logFile
+stat $?
+
+echo -n "Configuring Proxy:"
+cp expense.conf /etc/nginx/default.d/expense.conf  &>> $logFile
+stat $?
+
+echo -n "Clearning Old $component Content:"
+rm -rf /usr/share/nginx/html/* 
+stat $?
+
+echo -n "Downloading $component Content:"
+curl -o /tmp/$component.zip https://expense-web-app.s3.amazonaws.com/$component.zip &>> $logFile
+stat $?
+
+echo -n "Extracting $component Content:"
+cd /usr/share/nginx/html 
+unzip -o /tmp/$component.zip &>> $logFile
+stat $?
+
+echo -n "Restarting Nginx:"
+systemctl enable nginx   &>> $logFile
+systemctl restart nginx 
+stat $?
+
+echo -n "*****  $component Execution Completed  *****"
+
 
 
 

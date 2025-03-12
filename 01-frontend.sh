acf8cfd9760226ed1a7dfa1870bd34d1348578ed
YashavanthaN8057 <yme1061920@gmail.com>
2025-03-12 14:40:00 +0530

    doing exercise on if cond

--- 01-frontend.sh
+++ 01-frontend.sh
@@ -15,3 +15,20 @@ if [ $(id -u) -ne 0 ]; then
     else echo "this is root user"
     exit 3
  fi
+
+ 
+ vehAction= $1
+
+ if [ vehAction -eq start] ; then
+echo -e "\e[31m we are starting the vechicle \e[0m"
+
+else if [vehAction -eq stop] ; then
+echo -e "\e[31m we are stopping the vehicle according to the command \e[0m"
+
+else -e "\e[34m vehicles are waiting for the command\e[0m"
+
+fi
+echo -e "\e[35m doing execise on if conditions \e[0m"
+
+
+

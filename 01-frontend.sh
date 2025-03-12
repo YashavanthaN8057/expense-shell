27b386cb33f24b7cfa64c744c6bb23f60632efac
YashavanthaN8057 <yme1061920@gmail.com>
2025-03-12 15:05:37 +0530

    doing exercise on if cond

--- 01-frontend.sh
+++ 01-frontend.sh
@@ -25,9 +25,10 @@ echo -e "\e[31m we are starting the vechicle \e[0m"
 else if [vehAction -eq stop] ; then
 echo -e "\e[31m we are stopping the vehicle according to the command \e[0m"
 
-else -e "\e[34m vehicles are waiting for the command\e[0m"
+else  echo -e "\e[34m vehicles are waiting for the command\e[0m"
 
 fi
+
 echo -e "\e[35m doing execise on if conditions \e[0m"
 
 
